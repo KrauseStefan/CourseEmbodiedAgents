3cb8814c413e945c03b037a2a32f9287127f9a7a
theisnickelsen <theisnickelsen@gmail.com>
2014-02-06 13:07:58 +0100

    Read from light sensor code added

--- Lesson 1/src/Application.java
+++ Lesson 1/src/Application.java
@@ -1,9 +1,16 @@
+import lejos.nxt.*;
 
 public class Application {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		LightSensor light = new LightSensor(SensorPort.S1);
+		light.setFloodlight(true);
+		LCD.drawString("Light %:", 0, 0);
+		
+		while(! Button.LEFT.isDown()) {
+			LCD.drawInt(light.readValue(), 3, 9, 0);
+		}
 	}
 
 }
