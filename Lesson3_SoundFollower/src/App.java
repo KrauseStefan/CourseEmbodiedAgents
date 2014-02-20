85c1fda0b04bda0ab6896a08e62aea3ca0321a9a
STKKJ <stefan.krausekjaer@gmail.com>
2014-02-20 12:19:27 +0100

    Feature: Added datalogger to project

--- Lesson3_SoundFollower/src/App.java
+++ Lesson3_SoundFollower/src/App.java
@@ -1,37 +1,26 @@
 import lejos.nxt.*;
-/**
- * A simple sonar sensor test program.
- * 
- * The sensor should be connected to port 1. In the
- * known bugs and limitations of leJOS NXJ version alfa_03
- * it is mentioned that a gap of at least 300 msec is 
- * needed between calls of getDistance. This is the reason 
- * for the delay of 300 msec between sonar readings in the 
- * loop.
- * 
- * @author  Ole Caprani
- * @version 30.08.07
- */
+
 public class App {
 
-	   public static void main(String [] args)  
-			   throws Exception 
-			   {
-				   SoundSensor s = new SoundSensor(SensorPort.S2);
-				   int soundLevel;
+	private static PrjDataLogger prjLogger = new PrjDataLogger();
+	
+	public static void main(String[] args) throws Exception {
+		SoundSensor s = new SoundSensor(SensorPort.S2);
+		int soundLevel;
+		
+		prjLogger.connect();
+		
+		LCD.drawString("Level(dB) ", 0, 0);
 
-			       LCD.drawString("Level(dB) ", 0, 0);
-				   
-			       while (! Button.ESCAPE.isDown())
-			       {
-			           soundLevel = s.readValue();
-			           LCD.drawInt(soundLevel,3,9,0);
-
-			           Thread.sleep(5);
-			       }
-			       LCD.clear();
-			       LCD.drawString("Program stopped", 0, 0);
-			       Thread.sleep(2000);
-			   }
+		while (!Button.ESCAPE.isDown()) {
+			soundLevel = s.readValue();
+			LCD.drawInt(soundLevel, 3, 9, 0);
+			prjLogger.log(soundLevel);
+			Thread.sleep(5);
+		}
+		LCD.clear();
+		LCD.drawString("Program stopped", 0, 0);
+		Thread.sleep(2000);
+	}
 
 }
---
+++ Lesson3_SoundFollower/src/PrjDataLogger.java
@@ -0,0 +1,31 @@
+import java.io.IOException;
+
+import lejos.nxt.comm.Bluetooth;
+import lejos.nxt.comm.NXTConnection;
+import lejos.util.LogColumn;
+import lejos.util.NXTDataLogger;
+
+public class PrjDataLogger {
+	NXTDataLogger dlog = null;
+	NXTConnection conn = null;
+
+	PrjDataLogger() {
+		dlog = new NXTDataLogger();
+	}
+
+	void connect() throws IOException {
+		conn = Bluetooth.waitForConnection(0, NXTConnection.PACKET);
+		dlog.startRealtimeLog(conn);
+
+		dlog.setColumns(new LogColumn[] { new LogColumn("sensor", LogColumn.DT_INTEGER) });
+	}
+
+	void log(int sensorVal) throws InterruptedException {
+		dlog.writeLog(sensorVal);
+		dlog.finishLine();
+	}
+
+	protected void finalize() {
+		dlog.stopLogging();
+	}
+}
