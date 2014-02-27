9cba74601aeca943c7387a2ff135a45b6bb152d5
jacobhl <jacobhl@gmail.com>
2014-02-27 15:37:09 +0100

    PID

--- Lesson4_AdvLineFollower/src/App.java
+++ Lesson4_AdvLineFollower/src/App.java
@@ -3,10 +3,11 @@ public class App {
 
 	/**
 	 * @param args
+	 * @throws Exception 
 	 */
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
 		// TODO Auto-generated method stub
-
+		PIDLineFollower.main(null);
 	}
 
 }
