e8c6a043ce53a6e5eeb0104614540201f6b422a6
jacobhl <jacobhl@gmail.com>
2014-05-01 09:53:48 +0200

    arbitrator and behavior

--- lesson10_Sumobattle/src/BumperCar.java
+++ lesson10_Sumobattle/src/BumperCar.java
@@ -1,13 +1,5 @@
-import lejos.nxt.Button;
-import lejos.nxt.LCD;
-import lejos.nxt.Motor;
-import lejos.nxt.SensorPort;
-import lejos.nxt.TouchSensor;
-import lejos.nxt.UltrasonicSensor;
-//import lejos.robotics.MirrorMotor;
-import lejos.robotics.RegulatedMotor;
-import lejos.robotics.subsumption.Arbitrator;
-import lejos.robotics.subsumption.Behavior;
+
+import lejos.nxt.*;
 
 /**
  * Demonstration of the Behavior subsumption classes.
@@ -19,29 +11,29 @@ import lejos.robotics.subsumption.Behavior;
  * 
  * @author Brian Bagnall and Lawrie Griffiths, modified by Roger Glassey
  *
+ * Uses a new version of the Behavior interface and Arbitrator with
+ * integer priorities returned by takeCaontrol instead of booleans.
+ * 
+ * Exit behavior inserted, local distance sampling thread and
+ * backward drive added in DetectWall by Ole Caprani, 23-4-2012
  */
 public class BumperCar
 {
-  static RegulatedMotor leftMotor = Motor.A;
-  static RegulatedMotor rightMotor = Motor.C;
-  
-  // Use these definitions instead if your motors are inverted
-  // static RegulatedMotor leftMotor = MirrorMotor.invertMotor(Motor.A);
-  //static RegulatedMotor rightMotor = MirrorMotor.invertMotor(Motor.C);
-  
+
   public static void main(String[] args)
   {
-    leftMotor.setSpeed(400);
-    rightMotor.setSpeed(400);
+    Motor.A.setSpeed(400);
+    Motor.C.setSpeed(400);
     Behavior b1 = new DriveForward();
     Behavior b2 = new DetectWall();
+    Behavior b3 = new Exit();
     Behavior[] behaviorList =
     {
-      b1, b2
+      b1, b2, b3
     };
     Arbitrator arbitrator = new Arbitrator(behaviorList);
     LCD.drawString("Bumper Car",0,1);
-    Button.waitForAnyPress();
+    Button.waitForPress();
     arbitrator.start();
   }
 }
@@ -52,9 +44,9 @@ class DriveForward implements Behavior
 
   private boolean _suppressed = false;
 
-  public boolean takeControl()
+  public int takeControl()
   {
-    return true;  // this behavior always wants control.
+    return 10;  // this behavior always wants control.
   }
 
   public void suppress()
@@ -65,46 +57,118 @@ class DriveForward implements Behavior
   public void action()
   {
     _suppressed = false;
-    BumperCar.leftMotor.forward();
-    BumperCar.rightMotor.forward();
+    Motor.A.forward();
+    Motor.C.forward();
+    LCD.drawString("Drive forward",0,2);
     while (!_suppressed)
     {
       Thread.yield(); //don't exit till suppressed
     }
-    BumperCar.leftMotor.stop(); 
-    BumperCar.leftMotor.stop();
+    Motor.A.stop(); // not strictly necessary, but good programming practice
+    Motor.C.stop();
+    LCD.drawString("Drive stopped",0,2);
   }
 }
 
 
-class DetectWall implements Behavior
+class DetectWall extends Thread implements Behavior
 {
+  private boolean _suppressed = false;
+  private boolean active = false;
+  private int distance = 255;
 
   public DetectWall()
   {
     touch = new TouchSensor(SensorPort.S1);
     sonar = new UltrasonicSensor(SensorPort.S3);
+    this.setDaemon(true);
+    this.start();
+  }
+  
+  public void run()
+  {
+    while ( true ) distance = sonar.getDistance();
   }
 
-  public boolean takeControl()
+  public int takeControl()
   {
-    sonar.ping();
-    
-    return touch.isPressed() || sonar.getDistance() < 25;
+    if (touch.isPressed() || distance < 25)
+       return 100;
+    if ( active )
+       return 50;
+    return 0;
   }
 
   public void suppress()
   {
-    //Since  this is highest priority behavior, suppress will never be called.
+    _suppressed = true;// standard practice for suppress methods  
   }
 
   public void action()
   {
-    BumperCar.leftMotor.rotate(-180, true);// start Motor.A rotating backward
-    BumperCar.rightMotor.rotate(-360);  // rotate C farther to make the turn
+    _suppressed = false;
+    active = true;
+    Sound.beepSequenceUp();
+	
+    // Backward for 1000 msec
+    LCD.drawString("Drive backward",0,3);
+    Motor.A.backward();
+    Motor.C.backward();
+    int now = (int)System.currentTimeMillis();
+    while (!_suppressed && ((int)System.currentTimeMillis()< now + 1000) )
+    {
+       Thread.yield(); //don't exit till suppressed
+    }
+    
+    // Stop for 1000 msec
+    LCD.drawString("Stopped       ",0,3);
+    Motor.A.stop(); 
+    Motor.C.stop();
+    now = (int)System.currentTimeMillis();
+    while (!_suppressed && ((int)System.currentTimeMillis()< now + 1000) )
+    {
+      Thread.yield(); //don't exit till suppressed
+    }
+    
+    // Turn
+    LCD.drawString("Turn          ",0,3);
+    Motor.A.rotate(-180, true);// start Motor.A rotating backward
+    Motor.C.rotate(-360, true);  // rotate C farther to make the turn
+    while (!_suppressed && Motor.C.isMoving())
+    {
+      Thread.yield(); //don't exit till suppressed
+    }
+    Motor.A.stop(); 
+    Motor.C.stop();
+    LCD.drawString("Stopped       ",0,3);
+    Sound.beepSequence();
+    active = false;
+    
   }
-  
   private TouchSensor touch;
   private UltrasonicSensor sonar;
 }
+
+class Exit implements Behavior
+{
+  private boolean _suppressed = false;
+
+  public int takeControl()
+  {
+    if ( Button.ESCAPE.isPressed() )
+    	return 200;
+    return 0;
+  }
+
+  public void suppress()
+  {
+    _suppressed = true;// standard practice for suppress methods  
+  }
+
+  public void action()
+  {
+    System.exit(0);
+  }
+}
+
 
