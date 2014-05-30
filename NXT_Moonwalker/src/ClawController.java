410d4ece2a21078a3f02c1e08a5e8222926bed67
stkkj <stefan.krausekjaer@gmail.com>
2014-05-30 08:35:41 +0200

    Refacror(Claw): States are now a readable enum

--- NXT_Moonwalker/src/ClawController.java
+++ NXT_Moonwalker/src/ClawController.java
@@ -1,17 +1,23 @@
 import lejos.nxt.*;
 
 public class ClawController {
-	NXTRegulatedMotor motor;
-	int state = 0;
+	private NXTRegulatedMotor motor;
+	private ClawPositions state = ClawPositions.RELEASE;
 	
+	public enum ClawPositions {
+		RELEASE, //0 
+		LOAD, //1
+		CARRY //2
+	};
+
 	public ClawController(NXTRegulatedMotor m) {
-				
+
 		motor = m;
 		Button.LEFT.addButtonListener(new ButtonListener() {
 			@Override
 			public void buttonReleased(Button b) {
 			}
-			
+
 			@Override
 			public void buttonPressed(Button b) {
 				TurnClaw(-20, 70);
@@ -21,47 +27,53 @@ public class ClawController {
 			@Override
 			public void buttonReleased(Button b) {
 			}
-			
+
 			@Override
 			public void buttonPressed(Button b) {
-				TurnClaw(20,70);
+				TurnClaw(20, 70);
 			}
 		});
-		
+
 		Button.ENTER.addButtonListener(new ButtonListener() {
 			@Override
 			public void buttonReleased(Button b) {
 			}
-			
+
 			@Override
 			public void buttonPressed(Button b) {
 				setNextState();
 			}
-		});	
+		});
 	}
-	
+
 	public void TurnClaw(int deg, int speed) {
 		motor.setSpeed(speed);
 		motor.rotate(deg, true);
 	}
-	
-	public void TurnClawTo(int deg, int speed){
+
+	public void TurnClawTo(int deg, int speed) {
 		motor.setSpeed(speed);
 		motor.rotateTo(deg);
 	}
-	
-	public void SetClawTo(int newstate)
-	{
-		if(newstate == 0){TurnClawTo(0, 100);}
-		else if(newstate == 1){TurnClawTo(90, 80);}
-		else if(newstate == 2){TurnClawTo(170, 50);}
+
+	public void setState(ClawPositions newstate) {
+		state = newstate;
+		if (newstate == ClawPositions.RELEASE) {
+			TurnClawTo(0, 100);
+		} else if (newstate == ClawPositions.LOAD) {
+			TurnClawTo(90, 80);
+		} else if (newstate == ClawPositions.CARRY) {
+			TurnClawTo(170, 50);
+		}
 	}
-	
-	public void setNextState()
-	{
-		if(state!=2){SetClawTo(state+1);
-		state++;}
-		else {SetClawTo(0);
-		state=0;}
+
+	public void setNextState() {
+		if (state == ClawPositions.CARRY) {
+			setState(ClawPositions.RELEASE);
+		} else if (state == ClawPositions.LOAD){
+			setState(ClawPositions.CARRY);
+		} else if(state == ClawPositions.RELEASE){
+			setState(ClawPositions.LOAD);
+		}
 	}
 }
