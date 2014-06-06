69161dd7683521883fe74eee3654a5872d567b97
stkkj <stefan.krausekjaer@gmail.com>
2014-06-06 14:24:28 +0200

    Feature: LineFollower is now a runnable

--- NXT_Moonwalker/src/LineFollower.java
+++ NXT_Moonwalker/src/LineFollower.java
@@ -1,10 +1,8 @@
-import lejos.nxt.Button;
 import lejos.nxt.LCD;
-import lejos.nxt.SensorPort;
 
 
 
-public class LineFollower {
+public class LineFollower implements Runnable{
 	
 	private BlackWhiteSensor sensor;
 	private boolean isStarted;
@@ -14,19 +12,21 @@ public class LineFollower {
 		this.sensor = _sensor;
 		this.isStarted = false;
 		this.pilot = _pilot;
+
+		sensor.calibrate();
+
 	}
 	
-	public void start() throws Exception {
+	@Override
+	public void run() {
 		 this.isStarted = true;
 	     final int power = 300;
 		 final float Kp = 1.8f, Ki = 0, Kd = 0;
 		 float error, derivative, previous_error = 0, integral = 0, dt = 300;
 		 int sensorVal, powerDiff; 
-		 int powerLeft, powerRight;
-		 			 	
-	     sensor.calibrate();
-		 
-	     LCD.clear();
+//		 int powerLeft, powerRight;
+		 			 			 
+//	     LCD.clear();
 	     LCD.drawString("Light: ", 0, 2); 
 	     
 	     LCD.drawString("pDiff: ", 0, 4); 
@@ -41,35 +41,24 @@ public class LineFollower {
 			 derivative = (error - previous_error) / dt; //dt??
 			 powerDiff = (int) (Kp * error + Ki * integral + Kd * derivative);
 			 
-			 //powerLeft = power+powerDiff;
-			 //powerRight = power-powerDiff;
-			 /*
-			 if(powerDiff > 10)
-				 powerDiff = 10;
-			 else if (powerDiff < -10)
-				 powerDiff = -10;*/
 			 
 			 LCD.drawInt(sensorVal,4,10,2);
 			 LCD.drawInt(powerDiff, 4, 10, 4);
-			 //LCD.drawInt(powerRight,4,10,5);
 			 
 			 LCD.refresh();
-			// if ( sensor.isBlack() )
 		         pilot.forward(power-powerDiff, power+powerDiff);
-		    // else
-		    //	 pilot.forward(power+powerDiff,power+powerDiff);
 		     
 		     
-		     Thread.sleep(30);
-	     }
-	     
-	     //Car.stop();
-	     LCD.clear();
-	     LCD.drawString("Program stopped", 0, 0);
-	     LCD.refresh();
+		     try {
+				Thread.sleep(30);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+	     }	     
 	   }
 	
 	public void stop() {
 		this.isStarted = false;
 	}
+
 }
